84f6833e02ebffa7d8da90e5c0ceded1c4dbe3a1
Marcos Monteiro <marcos.moliveira@outlook.com>
2023-09-05 10:40:47 -0300

    criação da pagina de criação de usuario

--- public/routes/home/script/app.js
+++ public/routes/home/script/app.js
@@ -4,3 +4,33 @@ close.addEventListener('click', async () => {
     await localStorage.removeItem('token')
     window.location.href = '../../index.html'
 })
+
+// pega o token
+const token = localStorage.getItem('token')
+if(token == null) {
+    alert('Você não esta altenticado para essa rota!')
+    window.location.href = '../../index.html'
+}
+// decodifica o token
+const [headerEncoded, payloadEncoded] = token.split('.');
+const payload = JSON.parse(atob(payloadEncoded));
+
+// pega os elementos correspondente a nome e matricula
+const nome = document.getElementById('nome')
+const matriculaU = document.getElementById('mat')
+
+// seta nome e matricula nos elementos correspondentes
+nome.innerHTML = payload.nome
+matriculaU.innerHTML = payload.matricula
+
+function createUser(){
+    const create = document.getElementById('criarUser')
+
+    create.addEventListener('click', async () => {
+        if(payload.permission == true) {
+            window.location.href = '../createUser/create-user.html'
+        }
+    })
+}
+
+createUser()
