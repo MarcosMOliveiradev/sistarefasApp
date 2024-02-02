615fdba00806ce42f5c3fce564063d17264ca9d2
Marcos Monteiro <marcos.moliveira@outlook.com>
2024-02-02 08:59:22 -0300

    criando novas atividades

--- src/scripts/criarAtividade.js
+++ src/scripts/criarAtividade.js
@@ -1,3 +1,5 @@
+import { ENV_API } from "./utils/env.js"
+import { focar } from "./utils/focus.js"
 import { incrementarUmDia } from "./utils/formatData.js"
 
 class modal {
@@ -35,13 +37,71 @@ class modal {
 const modalNewActivy = new modal(".fechar", ".cadastro", ".newActivy", ".criarAtividade")
 
 modalNewActivy.init()
+focar()
+const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJub21lIjoiTWFyY29zIE1vbnRlaXJvICIsIm1hdHJpY3VsYSI6OTg0LCJwZXJtaXNzaW9uIjp0cnVlLCJ1c2VyQXZhdGEiOiJodHRwOi8vbG9jYWxob3N0OjMzMzMvdXBsb2FkL2Q5OGU5MjlkLTE2MGUtNDJhNi1hY2M2LTQ0OTEwZGRkNDRhNy5qcGciLCJzdWIiOiI4N2I0NTVkOC1hNjU2LTQzOGMtYjQyMy00Yjg2Zjk3NjNiNWMiLCJpYXQiOjE3MDY4NzI4MTksImV4cCI6MTcwNjk1OTIxOX0.rRBImY0jGQQwZT2AQeQWVY-2vDXUBwzQqMR5KyT1BrY'
 
 class CreatedNewActivity {
     constructor(enviar){
         this.enviar = document.getElementById(enviar)
+        this.codigo = document.getElementById('codigoInp')
     }
 
-    async apiCall(data, codigo, idDoc, qtdFolha, inicio, termino){
+    async getCode(){
+        this.codigo.addEventListener('keydown', async (event) => {
+            if(event.keyCode === 13 || event.keyCode === 9) {
+                const teste = parseInt(this.codigo.value)
+                try{
+                    let Api = ENV_API
+                    const response = await fetch(`${Api}/tasck/codigo?codigoTarefa=${teste}`, {
+                        method: 'GET',
+                        headers: {
+                            'Content-Type': 'application/json',
+                            'Authorization': `Bearer ${token}`
+                        }
+                    })
+        
+                    const responseAPI = await response.json()
+        
+                    responseAPI.forEach((objeto) => {
+                        const setor = document.getElementById('setor')
+                        const descricao = document.getElementById('descricao')
+
+                        setor.value = objeto.setor
+                        descricao.value = objeto.descricao
+        
+                        setor.classList.add('readonly')
+                        descricao.classList.add('readonly')
+                    });
+        
+                    const status = response.status()
+        
+                    if(status == 201) {
+                        location.reload()
+                    }
+                }catch(err){
+                    console.error('erro interno')
+                }
+            }
+        })
+    }
+
+    async apiCall(data, codigoTarefa, idDocumento, quantidadeFolhas, horaInicio, horaTermino){
+        const api = ENV_API
+        try{
+            const response = await fetch(`${api}/atividade`, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Authorization': `Bearer ${token}`
+                },
+                body: JSON.stringify({data, codigoTarefa, idDocumento, quantidadeFolhas, horaInicio, horaTermino})
+            })
+
+            alert(response.status)
+        }catch(err){
+            console.error(err)
+            throw err
+        }
 
     }
 
@@ -55,12 +115,18 @@ class CreatedNewActivity {
             const termino = document.getElementById('termino').value
 
             const data = incrementarUmDia(inputData)
+            const codigoTarefa = parseInt(codigo)
+
+            await this.apiCall(data, codigoTarefa, idDoc, qtdFolha, inicio, termino)
         })
     }
 
     init(){
         if (this.enviar) {
             this.addClickEvent()
+        }
+        if (this.codigo) {
+            this.getCode()
         }
         return this;
     }
